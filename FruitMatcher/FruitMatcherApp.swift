99401b8712fb37ddf31a4a2acafc7d3152e1bbca
MD. Sadil Khan <edwardstephendrood@gmail.com>
2021-06-09 21:35:14 +0200

    Initial Commit

---
+++ FruitMatcher/FruitMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMatcherApp.swift
+//  FruitMatcher
+//
+//  Created by Md. Sadil Khan on 09/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
